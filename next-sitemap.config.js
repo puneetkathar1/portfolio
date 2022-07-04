3987ce42bf246dedd1c880d1ab2d02e5f68a0223
puneetkathar1 <puneetkathar1@gmail.com>
2022-07-04 22:54:10 +0530

    bug

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "http://localhost:3000",
+  siteUrl: "https://puneetkathar.com",
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
